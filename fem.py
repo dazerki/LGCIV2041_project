d660e24608ca94beb987bad79fe5453a1a7a06fb
dazerki <antoine.quiriny@student.uclouvain.be>
2021-02-09 11:47:25 +0100

    geomet excpet rigid links

--- fem.py
+++ fem.py
@@ -1,1 +1,60 @@
 #usefull functions
+import numpy as np
+
+
+def geomet(namefile):
+    f = open(namefile, "r")
+    # Nnode, Nelement, Nmaterial and Nsection
+    f.readline()
+    line = f.readline()
+    line_split = line.split()
+    nNode = int(line_split[0])
+    nElement = int(line_split[1])
+    nMaterial = int(line_split[2])
+    nSection = int(line_split[3])
+
+    #coordinate
+    f.readline()
+    coord = np.empty((nNode,2))
+    for i in range(nNode):
+        line = f.readline()
+        line_split = line.split()
+        coord[i,0] = float(line_split[0])
+        coord[i,1] = float(line_split[1])
+
+    #connectivity DATA
+    f.readline()
+    IN = np.empty((nElement,2))
+    IMat = np.empty(nElement)
+    ISect = np.empty(nElement)
+    for i in range(nElement):
+        line = f.readline()
+        line_split = line.split()
+        IN[i,0] = int(line_split[0])
+        IN[i,1] = int(line_split[1])
+        IMat[i] = int(line_split[2])
+        ISect[i] = int(line_split[3])
+
+    #materials data
+    f.readline()
+    CMat = np.empty((nMaterial,4))
+    for i in range(nMaterial):
+        line = f.readline()
+        line_split = line.split()
+        CMat[i] = [float(j) for j in line_split]
+
+    #section data
+    f.readline()
+    CSect = np.empty((nSection,4))
+    for i in range(nSection):
+        line = f.readline()
+        line_split = line.split()
+        CSect[i] = [float(j) for j in line_split]
+
+    print(CSect[0,1])
+
+
+
+
+
+    return 0
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 #python file
+import numpy as np
+import fem
+
+if __name__ == "__main__":
+    #main code
+    #geomet
+    fem.geomet("FRAME01.txt")
